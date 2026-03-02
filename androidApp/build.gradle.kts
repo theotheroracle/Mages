fd6922a7f57c141623009f2163131d299544da6e
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2026-03-02 08:56:51 +0530

    chore: Update versionName from '3.2.2' to '4.0.0'

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 781
-        versionName = "3.2.2"
+        versionName = "4.0.0"
 
         // have to keep versionName here for fdroid, do not change
 
