b0035cc8501bd3aaa2119483fb4a2feed50540b9
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-03-25 14:51:36 +0530

    add the deleted toolchain resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositories {
         google {
